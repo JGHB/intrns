d4e7f7cf312fada1d1c9acb1e35b8bc2f242f6a7
JGHB <33645877+JGHB@users.noreply.github.com>
2023-08-28 13:39:41 -0400

    added more styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,12 @@
 import Header from "@/components/header";
-import SearchBox from "@/components/searchBox";
+import HomePageContent from "@/components/homePageContent";
 
 export default function Home() {
   return (
     <main className="">
       <Header />
       <div className="px-6 pt-9">
-        <h1 className="text-blue-600 font-semibold text-2xl">Startup Jobs</h1>
-        <h2 className="w-2/3 pb-3">
-          Help shape the future by joining one of the fastest growing technology
-          startups.
-        </h2>
-        <SearchBox />
+        <HomePageContent />
       </div>
     </main>
   );
--- components/searchBox.tsx
+++ components/searchBox.tsx
@@ -1,10 +1,10 @@
 const SearchBox = () => {
   return (
-    <div className="border-black w-full shadow-md rounded-lg ring-slate-300 ring-1 flex-wrap flex flex-row justify-between">
-      <div className="w-1/3 ">
+    <div className="border-black w-full shadow-md rounded-lg ring-slate-300 ring-1 flex-wrap flex flex-row justify-between p-0.5 h-32 items-stretch">
+      {/* <div className="w-full h-32 items-stretch">
         <div
           tabIndex={0}
-          className="focus-within:outline-none focus-within:ring focus-within:ring-violet-300 focus-within:text-blue-500"
+          className="focus-within:outline-none focus-within:ring focus-within:ring-blue-500 focus-within:text-blue-500 focus-within:ring-rounded-lg w-1/3 h-full p-3 rounded-lg"
         >
           <h1 className="focus-within:text-blue-500">
             What job are you looking for?
@@ -12,18 +12,61 @@ const SearchBox = () => {
           <input
             type="text"
             placeholder="Search"
-            className="text-black focus:text-black"
+            className="text-black focus:text-black w-full focus:outline-none text-2xl"
           />
         </div>
-      </div>
-      <div
+      </div> */}
+      {/* <div
         tabIndex={0}
-        className="p-8 text-center text-white bg-blue-500 cursor-pointer focus:border-4 focus:border-yellow-500"
+        className="focus-within:outline-none focus-within:ring focus-within:ring-violet-300 focus-within:text-blue-500 w-1/3"
       >
         Click me!
       </div>
-      <div>HELLO</div>
-      <div>HELLO</div>
+      <div
+        tabIndex={0}
+        className="focus-within:outline-none focus-within:ring focus-within:ring-violet-300 focus-within:text-blue-500 w-1/3"
+      >
+        HELLO
+      </div> */}
+      <div
+        tabIndex={0}
+        className="focus-within:outline-none focus-within:ring focus-within:ring-blue-500 focus-within:text-blue-500 focus-within:ring-rounded-lg w-1/3 h-full p-3 rounded-lg"
+      >
+        <h1 className="focus-within:text-blue-500">
+          What job are you looking for?
+        </h1>
+        <input
+          type="text"
+          placeholder="Search"
+          className="text-black focus:text-black w-full focus:outline-none text-2xl"
+        />
+      </div>
+      <div
+        tabIndex={0}
+        className="focus-within:outline-none focus-within:ring focus-within:ring-blue-500 focus-within:text-blue-500 focus-within:ring-rounded-lg w-1/3 h-full p-3 rounded-lg"
+      >
+        <h1 className="focus-within:text-blue-500">
+          What job are you looking for?
+        </h1>
+        <input
+          type="text"
+          placeholder="Search"
+          className="text-black focus:text-black w-full focus:outline-none text-2xl"
+        />
+      </div>
+      <div
+        tabIndex={0}
+        className="focus-within:outline-none focus-within:ring focus-within:ring-blue-500 focus-within:text-blue-500 focus-within:ring-rounded-lg w-1/3 h-full p-3 rounded-lg"
+      >
+        <h1 className="focus-within:text-blue-500">
+          What job are you looking for?
+        </h1>
+        <input
+          type="text"
+          placeholder="Search"
+          className="text-black focus:text-black w-full focus:outline-none text-2xl"
+        />
+      </div>
     </div>
   );
 };
